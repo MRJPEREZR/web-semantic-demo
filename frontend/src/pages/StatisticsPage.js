b97ea5de5d2dcd6c659b350a5c58866c7ce22ee4
SORO Yire Asma <yire.soro@imt-atlantique.net>
2024-11-29 02:07:27 +0100

    update w/no backend

--- frontend/src/pages/StatisticsPage.js
+++ frontend/src/pages/StatisticsPage.js
@@ -4,8 +4,70 @@ import React, { useState } from "react";
 import { FaTemperatureHigh, FaWind, FaPercentage, FaCloudRain } from 'react-icons/fa';
 import Bar from '../components/bar';
 
-const cities = [
-  "Paris", "London", "New York", "Tokyo", "Sydney"
+// Sample station data for selection
+const stations = [
+  { id: '07005', name: 'ABBEVILLE' },
+  { id: '07015', name: 'LILLE-LESQUIN' },
+  { id: '07020', name: 'PTE DE LA HAGUE' },
+  { id: '07027', name: 'CAEN-CARPIQUET' },
+  { id: '07037', name: 'ROUEN-BOOS' },
+  { id: '07072', name: 'REIMS-PRUNAY' },
+  { id: '07110', name: 'BREST-GUIPAVAS' },
+  { id: '07117', name: 'PLOUMANACH' },
+  { id: '07130', name: 'RENNES-ST JACQUES' },
+  { id: '07139', name: 'ALENCON' },
+  { id: '07149', name: 'ORLY' },
+  { id: '07168', name: 'TROYES-BARBEREY' },
+  { id: '07181', name: 'NANCY-OCHEY' },
+  { id: '07190', name: 'STRASBOURG-ENTZHEIM' },
+  { id: '07207', name: 'BELLE ILE-LE TALUT' },
+  { id: '07222', name: 'NANTES-BOUGUENAIS' },
+  { id: '07240', name: 'TOURS' },
+  { id: '07255', name: 'BOURGES' },
+  { id: '07280', name: 'DIJON-LONGVIC' },
+  { id: '07299', name: 'BALE-MULHOUSE' },
+  { id: '07314', name: 'PTE DE CHASSIRON' },
+  { id: '07335', name: 'POITIERS-BIARD' },
+  { id: '07434', name: 'LIMOGES-BELLEGARDE' },
+  { id: '07460', name: 'CLERMONT-FD' },
+  { id: '07471', name: 'LE PUY-LOUDES' },
+  { id: '07481', name: 'LYON-ST EXUPERY' },
+  { id: '07510', name: 'BORDEAUX-MERIGNAC' },
+  { id: '07535', name: 'GOURDON' },
+  { id: '07558', name: 'MILLAU' },
+  { id: '07577', name: 'MONTELIMAR' },
+  { id: '07591', name: 'EMBRUN' },
+  { id: '07607', name: 'MONT-DE-MARSAN' },
+  { id: '07621', name: 'TARBES-OSSUN' },
+  { id: '07627', name: 'ST GIRONS' },
+  { id: '07630', name: 'TOULOUSE-BLAGNAC' },
+  { id: '07643', name: 'MONTPELLIER' },
+  { id: '07650', name: 'MARIGNANE' },
+  { id: '07661', name: 'CAP CEPET' },
+  { id: '07690', name: 'NICE' },
+  { id: '07747', name: 'PERPIGNAN' },
+  { id: '07761', name: 'AJACCIO' },
+  { id: '07790', name: 'BASTIA' },
+  { id: '61968', name: 'GLORIEUSES' },
+  { id: '61970', name: 'JUAN DE NOVA' },
+  { id: '61972', name: 'EUROPA' },
+  { id: '61976', name: 'TROMELIN' },
+  { id: '61980', name: 'GILLOT-AEROPORT' },
+  { id: '61996', name: 'NOUVELLE AMSTERDAM' },
+  { id: '61997', name: 'CROZET' },
+  { id: '61998', name: 'KERGUELEN' },
+  { id: '67005', name: 'PAMANDZI' },
+  { id: '71805', name: 'ST-PIERRE' },
+  { id: '78890', name: 'LA DESIRADE METEO' },
+  { id: '78894', name: 'ST-BARTHELEMY METEO' },
+  { id: '78897', name: 'LE RAIZET AERO' },
+  { id: '78922', name: 'TRINITE-CARAVEL' },
+  { id: '78925', name: 'LAMENTIN-AERO' },
+  { id: '81401', name: 'SAINT LAURENT' },
+  { id: '81405', name: 'CAYENNE-MATOURY' },
+  { id: '81408', name: 'SAINT GEORGES' },
+  { id: '81415', name: 'MARIPASOULA' },
+  { id: '89642', name: 'DUMONT D URVILLE' }
 ];
 
 const months = [
@@ -13,8 +75,8 @@ const months = [
 ];
 
 function StatisticsPage() {
-    const [city, setCity] = useState("Paris");
-    const [month, setMonth] = useState(0);  // Index du mois (0 = Janvier)
+    const [selectedStation, setSelectedStation] = useState('');
+    const [month, setMonth] = useState('');  // Index du mois (0 = Janvier)
     const [weatherData, setWeatherData] = useState({
       highestTemp: 25,
       lowestTemp: 10,
@@ -22,7 +84,12 @@ function StatisticsPage() {
       lowestDay: "22/01/24",
       averageTemp: 22,
     });
-  
+
+      // Handle selection of station
+    const handleStationChange = (e) => {
+      setSelectedStation(e.target.value);
+    };
+    
 
   
     return (
@@ -30,10 +97,11 @@ function StatisticsPage() {
          <div className="selector">
           <div>
             <label >Choose a station:</label>
-            <select value={city} onChange={(e) => setCity(e.target.value)}>
-              {cities.map((cityName) => (
-                <option key={cityName} value={cityName}>
-                  {cityName}
+            <select id="station" value={selectedStation} onChange={handleStationChange}>
+            <option value="">-- Select a Station --</option>
+              {stations.map((station) => (
+                <option key={station.id} value={station.id}>
+                  {station.name}
                 </option>
               ))}
             </select>
@@ -50,7 +118,7 @@ function StatisticsPage() {
             </select>
           </div>
         </div>
-        <h1>Weather forecast for {city} station</h1>
+        <h1>Weather forecast for {stations.find((s) => s.id === selectedStation)?.name} station</h1>
   
        
   
@@ -60,12 +128,15 @@ function StatisticsPage() {
             <div className='result2'>
               <div className='box' >
                 <div className="col2"><FaTemperatureHigh/><p >Temperature</p></div>
+                
                 <h4 className='p3'>mean:</h4>
+                {/* put here the average temparature for the city selected*/}
                 <Bar/>
               </div> 
               <div className='box'>
                 <div className="col2"><FaPercentage/><p>Humidity</p></div>
                   <h4>mean:</h4>
+                  {/* put here the average humidity for the city selected*/}
                   <Bar/>
                 
               </div>
@@ -74,6 +145,7 @@ function StatisticsPage() {
                   <FaCloudRain/><p >Pressure</p>
                 </div>
                 <h4>mean:</h4>
+                {/* put here the average pressure for the city selected*/}
                 <Bar/>
               </div> 
               <div className='box'>
@@ -81,6 +153,7 @@ function StatisticsPage() {
                   <FaWind/><p >Wind Speed</p>
                 </div>
                 <h4>mean:</h4>
+                {/* put here the average wind speed for the city selected*/}
                 <Bar/>
               </div> 
             </div>
